0623d23db83cf3b4a2294cf10e8d7ae5669fd8ac
swatper <67682393+swatper@users.noreply.github.com>
2025-04-10 12:51:28 +0900

    jacoco 의존성 추가

--- spring_webmail/src/test/java/deu/cse/spring_webmail/SpringWebmailApplicationTests.java
+++ spring_webmail/src/test/java/deu/cse/spring_webmail/SpringWebmailApplicationTests.java
@@ -5,9 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringWebmailApplicationTests {
-
+    
+        //TODO
+        //@Test를 사용해서 유닛 단위 테스트(junit) 메서드 작성
 	@Test
 	void contextLoads() {
 	}
 
+        //jacoco는 이러한 단위 테스트들을 총괄해서 평가
 }
